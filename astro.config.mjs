d4293f8a9cfca7e2c2bf4d573174ca75424fcb19
Vishal <achilles3x@gmail.com>
2025-02-19 00:59:18 +0530

    Second commit yaay

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import node from "@astrojs/node";
+import netlify from '@astrojs/netlify';
+ // Use the correct Netlify adapter
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "server", // Change this from "server" to "static" for Netlify
+  adapter: netlify(),
+  site: "https://binaryvish.netlify.app", // Add your Netlify site URL
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
